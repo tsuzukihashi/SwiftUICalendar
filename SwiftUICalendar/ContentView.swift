cd38370e10b7ef0df9372f4099e0a97d42960d58
tsuzukihashi <tsudukihashi0817@gmail.com>
2019-12-03 00:37:28 +0900

    Initial Commit

---
+++ SwiftUICalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICalendar
+//
+//  Created by Ryo Tsuzukihashi on 2019/12/03.
+//  Copyright © 2019 Ryo Tsuzukihashi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
